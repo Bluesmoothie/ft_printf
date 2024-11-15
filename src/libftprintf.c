6d171a36ec8c979efdb593a36c475421ae13aa42
Yves Gille <ygille@student.42lyon.fr>
2024-11-15 13:01:00 +0100

    Ajout du support de %s

--- src/libftprintf.c
+++ src/libftprintf.c
@@ -6,13 +6,51 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:42:13 by ygille            #+#    #+#             */
-/*   Updated: 2024/11/15 11:03:49 by ygille           ###   ########.fr       */
+/*   Updated: 2024/11/15 12:52:29 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/libftprintf.h"
+#include "../include/libftprintf.h"
+
+static	int	handle_percent(const char *format, va_list *ap, int *i);
 
 int	ft_printf(const char *format, ...)
 {
+	va_list	ap;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
+	va_start(ap, format);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			ft_putchar_fd(format[i], 1);
+			j++;
+			i++;
+		}
+		else
+			j += handle_percent(format, &ap, &i);
+	}
+	va_end(ap);
+	return (j);
+}
+
+static int	handle_percent(const char *format, va_list *ap, int *i)
+{
+	(*i) += 1;
+	if (format[*i] == '%')
+		return (print_percent(i));
+	else if (format[*i] == 'c')
+		return (print_char(i, ap));
+	else if (format[*i] == 's')
+		return (print_string(i, ap));
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	return (ft_printf(argv[1], argv[2]));
 }
--- src/printer_1.c
+++ src/printer_1.c
@@ -1,24 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   printer_1.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/14 17:36:59 by ygille            #+#    #+#             */
-/*   Updated: 2024/11/15 12:52:55 by ygille           ###   ########.fr       */
+/*   Created: 2024/11/15 12:49:38 by ygille            #+#    #+#             */
+/*   Updated: 2024/11/15 12:54:05 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+#include "../include/libftprintf.h"
 
-# include "../libft/libft.h"
-# include <stdarg.h>
+int	print_percent(int *i)
+{
+	ft_putchar_fd('%', 1);
+	(*i) += 1;
+	return (1);
+}
 
-int	ft_printf(const char *format, ...);
-int	print_percent(int *i);
-int	print_char(int *i, va_list *ap);
-int	print_string(int *i, va_list *ap);
+int	print_char(int *i, va_list *ap)
+{
+	ft_putchar_fd(*(va_arg(*ap, char *)), 1);
+	(*i) += 1;
+	return (1);
+}
 
-#endif
+int	print_string(int *i, va_list *ap)
+{
+	char	*s;
+
+	s = va_arg(*ap, char *);
+	ft_putstr_fd(s, 1);
+	(*i) += 1;
+	return (ft_strlen(s));
+}
