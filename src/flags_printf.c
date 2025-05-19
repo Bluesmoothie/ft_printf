8f776e43d586769463dfa9df100bac5aa9e0c61f
Yves Gille <ygille@student.42lyon.fr>
2025-05-19 14:14:27 +0200

    Final stage : in accordance with printf man but not with printf behaviour There are differents

--- src/flags_printf.c
+++ src/flags_printf.c
@@ -6,20 +6,21 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 12:29:23 by ygille            #+#    #+#             */
-/*   Updated: 2025/05/19 13:20:14 by ygille           ###   ########.fr       */
+/*   Updated: 2025/05/19 14:13:49 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int		ft_putstr_fd_adjust_alpha(t_flags flags, char *str, int len, int fd);
-static int		ft_putstr_fd_adjust_numeric(t_flags flags, char *str, int len, int fd);
-static t_flags	modif_flags(t_flags flags, int len);
+static int		ft_putstr_fd_adjust_alpha(t_flags flags, char *str,
+					int len, int fd);
+static int		ft_putstr_fd_adjust_numeric(t_flags flags, char *str,
+					int len, int fd);
 
 int	ft_putstr_fd_adjust(t_flags flags, char *str, int len, int fd)
 {
 	if (is_numeric(flags.format))
-		return (ft_putstr_fd_adjust_numeric(modif_flags(flags, len), str, len, fd));
+		return (ft_putstr_fd_adjust_numeric(flags, str, len, fd));
 	return (ft_putstr_fd_adjust_alpha(flags, str, len, fd));
 }
 
@@ -33,7 +34,7 @@ static int	ft_putstr_fd_adjust_alpha(t_flags flags, char *str, int len, int fd)
 	len = ft_trunc(flags, &str, len);
 	if (!str)
 		return (0);
-	if (flags.right_just || flags.left_just)
+	if (flags.right_just)
 		p += adjust(flags.right_just, len, ' ', fd);
 	ft_putstr_fd(&str[i], fd);
 	if (flags.left_just)
@@ -42,27 +43,28 @@ static int	ft_putstr_fd_adjust_alpha(t_flags flags, char *str, int len, int fd)
 	return (p + len);
 }
 
-static int	ft_putstr_fd_adjust_numeric(t_flags flags, char *str, int len, int fd)
+static int	ft_putstr_fd_adjust_numeric(t_flags flags, char *str,
+		int len, int fd)
 {
-	int	p;
-	int	i;
+	int		p;
+	bool	sign;
 
 	p = 0;
-	i = 0;
-	if (flags.accuracy > len)
-		p += adjust(flags.accuracy, len, '0', fd);
+	sign = false;
 	if (flags.right_just)
 		p += adjust(flags.right_just, len, ' ', fd);
-	else if (flags.zeros)
+	if (*str == '-')
+	{
+		ft_putchar_fd(*str, fd);
+		str++;
+		sign = true;
+	}
+	if (!flags.right_just && flags.accuracy == -1)
 		p += adjust(flags.zeros, len, '0', fd);
-	ft_putstr_fd(&str[i], fd);
+	else if (flags.accuracy != -1)
+		p += adjust(flags.accuracy, len - sign, '0', fd);
+	ft_putstr_fd(str, fd);
 	if (flags.left_just)
 		p += adjust(flags.left_just, len, ' ', fd);
 	return (p + len);
-}
-
-static t_flags	modif_flags(t_flags flags, int len)
-{
-	(void)len;
-	return (flags);
 }
